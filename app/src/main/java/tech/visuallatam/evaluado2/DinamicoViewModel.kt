4f7f9bfd99dd4e01c3fa16e2c455ca7857ed04cd
hscanales <00136317@uca.edu.sv>
2019-05-03 10:19:01 -0600

    siuuuuu

--- app/src/main/java/tech/visuallatam/evaluado2/DinamicoViewModel.kt
+++
@@ -1,7 +0,0 @@
-package tech.visuallatam.evaluado2
-
-import android.arch.lifecycle.ViewModel;
-
-class DinamicoViewModel : ViewModel() {
-    // TODO: Implement the ViewModel
-}
--- app/src/main/java/tech/visuallatam/evaluado2/MainActivity.kt
+++ app/src/main/java/tech/visuallatam/evaluado2/MainActivity.kt
@@ -2,11 +2,69 @@ package tech.visuallatam.evaluado2
 
 import android.support.v7.app.AppCompatActivity
 import android.os.Bundle
+import kotlinx.android.synthetic.main.activity_main.*
 
-class MainActivity : AppCompatActivity() {
+
+class MainActivity() : AppCompatActivity(), Botones.OnSelectOption {
+
+    var imagenes : ArrayList<Int> = ArrayList()
+    var actual : Int? = null
+
+
+    override fun onAction(id: Int) {
+        var content = when (id) {
+            1 -> {
+                if (actual == R.drawable.a) {
+                    R.drawable.c
+
+                } else if(actual == R.drawable.b) {
+                    R.drawable.a
+
+                }else{
+                    R.drawable.b
+                }
+
+            }
+            2 -> {
+                if (actual == R.drawable.a) {
+                    R.drawable.b
+
+                } else if(actual == R.drawable.b) {
+                    R.drawable.c
+
+                }else{
+                    R.drawable.a
+                }
+            }
+
+            else -> {
+                imagenes.last()
+            }
+        }
+        actual = content
+
+        supportFragmentManager.beginTransaction()
+                .replace(R.id.fragment_content, ContentFragment.newInstance(content))
+                .addToBackStack("Co")
+                .commit()
+    }
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+        imagenes.add(R.drawable.a)
+        imagenes.add(R.drawable.b)
+        imagenes.add(R.drawable.c)
+
+        var barFragment = Botones.newInstace(
+                "Op1",
+                "Op2"
+        )
+        if (savedInstanceState == null) {
+            supportFragmentManager
+                    .beginTransaction()
+                    .add(R.id.fragment_bar, barFragment)
+                    .commit()
+        }
     }
 }
